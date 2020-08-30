43bd4e1abca483aa37e83336cdc84402da12de59
&y Haskell <andy.with.ampersand@gmail.com>
2020-08-30 12:15:32 -0400

    add code for the final product of the tutorial

---
+++ final-product/src/components/ColorPicker.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <h3> Select a color</h3>
+    <radial-color-picker
+      aria-label="hue radial selector"
+      @input="selectColor"
+    />
+  </form>
+</template>
+
+<script>
+import ColorPicker from '@radial-color-picker/vue-color-picker';
+
+export default {
+  name: 'ColorPicker',
+  components: {
+    'radial-color-picker': ColorPicker
+  },
+  methods: {
+    selectColor(hue) {
+      this.$emit('color-change', `hsl(${hue}, 100%, 50%)`);
+    },
+  },
+}
+</script>
+
+<style>
+@import '~@radial-color-picker/vue-color-picker/dist/vue-color-picker.min.css';
+</style>
