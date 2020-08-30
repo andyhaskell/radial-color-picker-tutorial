ebf5d4fcf907126687aa13d43b78542c6550e1af
&y Haskell <andy.with.ampersand@gmail.com>
2020-08-30 11:52:42 -0400

    add starting code to follow along with

---
+++ follow-along/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="whole-thing">
+    <h1 :style="{color: color}">{{ message }}</h1>
+    <div aria-label="message input">
+      <input type="text" v-model="message" />
+    </div>
+    <color-picker
+      aria-label="color picker"
+      @color-change="colorChange"
+      :colors="['black', 'red', 'green', 'blue']"
+    ></color-picker>
+  </div>
+</template>
+
+<script>
+import ColorPicker from './components/ColorPicker.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'color-picker': ColorPicker,
+  },
+
+  data() {
+    return {
+      color: 'black',
+      message: 'write a message',
+    };
+  },
+
+  methods: {
+    colorChange(color) {
+      this.color = color;
+    },
+  },
+}
+</script>
---
+++ follow-along/src/components/ColorPicker.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+    <h3> Select a color</h3>
+    <button
+      v-for="color in colors"
+      :key="color"
+      @click.prevent="selectColor(color)"
+    >
+      {{ color }}
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+  props: ['colors'],
+  methods: {
+    selectColor(color) {
+      this.$emit('color-change', color);
+    },
+  },
+}
+</script>
